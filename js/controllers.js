7c33423072934780b572b10348f4e926afb20e58
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2016-01-06 10:17:01 -0600

    Added filtering and ordering.

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,6 @@ var testing = angular.module('testing',['ngSanitize']);
 testing.controller('MDListing', function MDListing($scope, $http){
 	$http.get('data.json').success(function (data){
 		$scope.mdItems = data;
+		$scope.orderField = "type";
 	});
 });
