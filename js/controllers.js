d8a69dacb32a33d0a3cee859d050c5ba48eb393c
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2016-01-06 09:52:11 -0600

    True data logic -- repeats on test data.

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-var testing = angular.module('testing',[]);
+var testing = angular.module('testing',['ngSanitize']);
 
 testing.controller('MDListing', function MDListing($scope){
 
@@ -7,5 +7,15 @@ testing.controller('MDListing', function MDListing($scope){
 		"md" : "`var f = new function()`",
 		"html" : "<code>var f = new function()</code>"
 	}
+	$scope.mdItems = [{
+		"type" : "Code",
+		"md" : "`var f = new function()`",
+		"html" : "<code>var f = new function()</code>"
+	},
+	{
+		"type" : "H1",
+		"md" : "# This is a Header 1",
+		"html" : "<h1>This is a Header 1</h1>"
+	}]
 
 });
