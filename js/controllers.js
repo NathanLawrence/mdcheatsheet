b40c1889b0057323ae2582b755173dcc3653dfd8
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2016-01-06 10:04:52 -0600

    Pulling the actual data!

--- js/controllers.js
+++ js/controllers.js
@@ -1,21 +1,7 @@
 var testing = angular.module('testing',['ngSanitize']);
 
-testing.controller('MDListing', function MDListing($scope){
-
-	$scope.entry = {
-		"type" : "Code",
-		"md" : "`var f = new function()`",
-		"html" : "<code>var f = new function()</code>"
-	}
-	$scope.mdItems = [{
-		"type" : "Code",
-		"md" : "`var f = new function()`",
-		"html" : "<code>var f = new function()</code>"
-	},
-	{
-		"type" : "H1",
-		"md" : "# This is a Header 1",
-		"html" : "<h1>This is a Header 1</h1>"
-	}]
-
+testing.controller('MDListing', function MDListing($scope, $http){
+	$http.get('data.json').success(function (data){
+		$scope.mdItems = data;
+	});
 });
